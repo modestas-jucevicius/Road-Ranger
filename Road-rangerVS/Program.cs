f2668b0c6e807570649fa31841e3d63592b87a05
aurisgo <aurimas.golotylecas@gmail.com>
2018-09-16 13:05:02 +0300

    Added Car Image Recognition

--- Road-rangerVS/Program.cs
+++ Road-rangerVS/Program.cs
@@ -1,22 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text;
+using System.Net.Http;
+using System.Diagnostics;
 
 namespace Road_rangerVS
 {
 	static class Program
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+		static  void Main()
 		{
-			Application.EnableVisualStyles();
+            RecognitionTest();
+
+            Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new Form1());
-		}
-	}
+        }
+
+        static async Task RecognitionTest()
+        {
+            string imagePath = "vilnius-zeneva-su-volvo-xc60-5314ead086571.jpg";
+            imagePath = Path.Combine(Environment.CurrentDirectory, @"images\", imagePath);
+            Console.WriteLine("Full Path: {0}", imagePath);
+
+            Recognizer recognizer = new OpenALPRRecognizer();
+            string result = await recognizer.Recognize(imagePath);
+
+            Parser parser = new OpenALPRParser();
+            if (!parser.IsError(result))
+            {
+                List<ParsedCar> cars = parser.Parse(result);
+
+                foreach (ParsedCar car in cars)
+                {
+                    car.Display();
+                }
+            }
+            else
+                Console.WriteLine("Error message!");
+            
+        }
+    }
 }
---
+++ Road-rangerVS/Recognition/OpenALPRParser.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Road_rangerVS
+{
+    class OpenALPRParser : Parser
+    {
+        public List<ParsedCar> Parse(string data)
+        {
+            JObject o = JObject.Parse(data);
+
+            JArray results = (JArray)o["results"];
+
+            List<ParsedCar> cars = new List<ParsedCar>();
+
+            foreach (JObject result in results)
+            {
+                string plate = (string)result["plate"];
+
+                JObject vehicle = (JObject)result["vehicle"];
+
+                JArray colors = (JArray)vehicle["color"];
+                JObject colorObj = (JObject)colors[0];
+                string colorName = (string)colorObj["name"];
+
+                JArray makes = (JArray)vehicle["make"];
+                JObject makeObj = (JObject)makes[0];
+                string makeName = (string)makeObj["name"];
+
+                JArray bodies = (JArray)vehicle["body_type"];
+                JObject bodyObj = (JObject)bodies[0];
+                string bodyType = (string)bodyObj["name"];
+
+                JArray years = (JArray)vehicle["year"];
+                JObject yearObj = (JObject)years[0];
+                string year = (string)yearObj["name"];
+
+                JArray models = (JArray)vehicle["make_model"];
+                JObject modelObj = (JObject)models[0];
+                string model = (string)modelObj["name"];
+
+                cars.Add(new ParsedCar(plate, colorName, makeName, model, bodyType, year));
+            }
+
+            return cars;
+        }
+
+        public bool IsError(string data)
+        {
+            JObject o = JObject.Parse(data);
+            JToken value;
+            if (o.TryGetValue("error_code", out value))
+                return true;
+            else
+                return false;
+        }
+    }
+}
