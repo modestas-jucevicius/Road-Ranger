7f5571579cb91c46f448460f7d7e5fd6cfcd0137
aurisgo <aurimas.golotylecas@gmail.com>
2018-09-16 17:29:24 +0300

    Added Recognition UI

--- Road-rangerVS/Main Form.Designer.cs
+++ Road-rangerVS/Main Form.Designer.cs
@@ -28,36 +28,75 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.label1 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(248, 72);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(289, 51);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Road Ranger";
-			this.label1.Click += new System.EventHandler(this.label1_Click);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.label1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.upload = new System.Windows.Forms.Button();
+            this.browseImage = new System.Windows.Forms.Button();
+            this.filePath = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(248, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Road Ranger";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // upload
+            // 
+            this.upload.Location = new System.Drawing.Point(280, 257);
+            this.upload.Name = "upload";
+            this.upload.Size = new System.Drawing.Size(208, 23);
+            this.upload.TabIndex = 1;
+            this.upload.Text = "Upload";
+            this.upload.UseVisualStyleBackColor = true;
+            this.upload.Click += new System.EventHandler(this.UploadButtonClick);
+            // 
+            // browseImage
+            // 
+            this.browseImage.Location = new System.Drawing.Point(280, 178);
+            this.browseImage.Name = "browseImage";
+            this.browseImage.Size = new System.Drawing.Size(208, 23);
+            this.browseImage.TabIndex = 2;
+            this.browseImage.Text = "Browse Image";
+            this.browseImage.UseVisualStyleBackColor = true;
+            this.browseImage.Click += new System.EventHandler(this.BrowseButtonClick);
+            // 
+            // filePath
+            // 
+            this.filePath.AutoSize = true;
+            this.filePath.Location = new System.Drawing.Point(277, 220);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(16, 13);
+            this.filePath.TabIndex = 3;
+            this.filePath.Text = "...";
+            this.filePath.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.browseImage);
+            this.Controls.Add(this.upload);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.Label label1;
-	}
+        private System.Windows.Forms.Button upload;
+        private System.Windows.Forms.Button browseImage;
+        private System.Windows.Forms.Label filePath;
+    }
 }
 
--- Road-rangerVS/Main Form.cs
+++ Road-rangerVS/Main Form.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,5 +22,55 @@ namespace Road_rangerVS
 		{
 
 		}
-	}
+
+        private void UploadButtonClick(object sender, EventArgs e)
+        {
+            string imagePath = filePath.Text;
+            if (!File.Exists(imagePath))
+                MessageBox.Show("Wrong Image Path!");
+            else
+                Recognize(imagePath);
+        }
+
+        static async Task Recognize(string imagePath)
+        {
+            Console.WriteLine("Full Path: {0}", imagePath);
+
+            Recognizer recognizer = new OpenALPRRecognizer();
+            string result = await recognizer.Recognize(imagePath);
+
+            Parser parser = new OpenALPRParser();
+            if (!parser.IsError(result))
+            {
+                List<ParsedCar> cars = parser.Parse(result);
+
+                foreach (ParsedCar car in cars)
+                {
+                    car.Display();
+                }
+
+                if (cars.Count() == 0)
+                    MessageBox.Show("Wrong Image!");
+            }
+            else
+                MessageBox.Show("Wrong Image!");
+        }
+
+        private void BrowseButtonClick(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files | *.png; *.jpg"; // file types, that will be allowed to upload
+            dialog.Multiselect = false; // allow/deny user to upload more than one file at a time
+            if (dialog.ShowDialog() == DialogResult.OK) // if user clicked OK
+            {
+                String path = dialog.FileName; // get name of file
+                filePath.Text = path;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
