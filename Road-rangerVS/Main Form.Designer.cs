e0022142bedb698dca25414b674f48eae1e205ff
PauliusAn <pauliusanuzas@gmail.com>
2018-09-21 10:12:06 +0300

    Added camera input

--- Road-rangerVS/Main Form.Designer.cs
+++ Road-rangerVS/Main Form.Designer.cs
@@ -28,66 +28,105 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.label1 = new System.Windows.Forms.Label();
-			this.upload = new System.Windows.Forms.Button();
-			this.browseImage = new System.Windows.Forms.Button();
-			this.filePath = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(248, 72);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(289, 51);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Road Ranger";
-			this.label1.Click += new System.EventHandler(this.label1_Click);
-			// 
-			// upload
-			// 
-			this.upload.Location = new System.Drawing.Point(280, 216);
-			this.upload.Name = "upload";
-			this.upload.Size = new System.Drawing.Size(208, 23);
-			this.upload.TabIndex = 1;
-			this.upload.Text = "Upload";
-			this.upload.UseVisualStyleBackColor = true;
-			this.upload.Click += new System.EventHandler(this.UploadButtonClick);
-			// 
-			// browseImage
-			// 
-			this.browseImage.Location = new System.Drawing.Point(280, 145);
-			this.browseImage.Name = "browseImage";
-			this.browseImage.Size = new System.Drawing.Size(208, 23);
-			this.browseImage.TabIndex = 2;
-			this.browseImage.Text = "Browse Image";
-			this.browseImage.UseVisualStyleBackColor = true;
-			this.browseImage.Click += new System.EventHandler(this.BrowseButtonClick);
-			// 
-			// filePath
-			// 
-			this.filePath.AutoSize = true;
-			this.filePath.Location = new System.Drawing.Point(277, 188);
-			this.filePath.Name = "filePath";
-			this.filePath.Size = new System.Drawing.Size(16, 13);
-			this.filePath.TabIndex = 3;
-			this.filePath.Text = "...";
-			this.filePath.Click += new System.EventHandler(this.label2_Click);
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.filePath);
-			this.Controls.Add(this.browseImage);
-			this.Controls.Add(this.upload);
-			this.Controls.Add(this.label1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.upload = new System.Windows.Forms.Button();
+            this.browseImage = new System.Windows.Forms.Button();
+            this.filePath = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(246, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Road Ranger";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // upload
+            // 
+            this.upload.Location = new System.Drawing.Point(280, 144);
+            this.upload.Name = "upload";
+            this.upload.Size = new System.Drawing.Size(208, 23);
+            this.upload.TabIndex = 1;
+            this.upload.Text = "Upload";
+            this.upload.UseVisualStyleBackColor = true;
+            this.upload.Click += new System.EventHandler(this.UploadButtonClick);
+            // 
+            // browseImage
+            // 
+            this.browseImage.Location = new System.Drawing.Point(280, 102);
+            this.browseImage.Name = "browseImage";
+            this.browseImage.Size = new System.Drawing.Size(208, 23);
+            this.browseImage.TabIndex = 2;
+            this.browseImage.Text = "Browse Image";
+            this.browseImage.UseVisualStyleBackColor = true;
+            this.browseImage.Click += new System.EventHandler(this.BrowseButtonClick);
+            // 
+            // filePath
+            // 
+            this.filePath.AutoSize = true;
+            this.filePath.Location = new System.Drawing.Point(277, 128);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(16, 13);
+            this.filePath.TabIndex = 3;
+            this.filePath.Text = "...";
+            this.filePath.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(280, 173);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(208, 166);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "&Camera";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(280, 345);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(208, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.browseImage);
+            this.Controls.Add(this.upload);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -97,6 +136,9 @@
         private System.Windows.Forms.Button upload;
         private System.Windows.Forms.Button browseImage;
         private System.Windows.Forms.Label filePath;
-	}
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
 }
 
--- Road-rangerVS/Main Form.cs
+++ Road-rangerVS/Main Form.cs
@@ -9,10 +9,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using AForge.Video;
+using AForge.Video.DirectShow;
+
 namespace Road_rangerVS
 {
 	public partial class Form1 : Form
 	{
+        private FilterInfoCollection VideoCaptureDevices;
+        private VideoCaptureDevice FinalVideo;
 		public Form1()
 		{
 			InitializeComponent();
@@ -79,5 +84,43 @@ namespace Road_rangerVS
         {
 
         }
-	}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo VideoCaptureDevice in VideoCaptureDevices)
+            {
+                comboBox1.Items.Add(VideoCaptureDevice.Name);
+            }
+
+            comboBox1.SelectedIndex = 0;
+            FinalVideo = new VideoCaptureDevice();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (FinalVideo.IsRunning == true) FinalVideo.Stop();
+
+            FinalVideo = new VideoCaptureDevice(VideoCaptureDevices[comboBox1.SelectedIndex].MonikerString);
+            FinalVideo.NewFrame += FinalVideo_NewFrame;
+            FinalVideo.Start();
+        }
+
+        private void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap video = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox2.Image = video;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (FinalVideo.IsRunning == true) FinalVideo.Stop();
+        }
+    }
 }
