08b06892bd79cf2093743d1d4a1916a811bcb61b
PauliusAn <pauliusanuzas@gmail.com>
2018-10-14 10:33:05 +0300

    Added Score class, added it to the FileSystem

---
+++ Road-rangerVS/Score/Score.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Road_rangerVS.Score
+{
+    class Score
+    {
+        public int userID { get; set; }
+        public int score { get; set; }
+    }
+}
