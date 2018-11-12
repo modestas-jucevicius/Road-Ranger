413205d21861ed213eb851d0ef55a9907085e5fb
PauliusAn <pauliusanuzas@gmail.com>
2018-11-12 20:18:07 +0200

    Some changes on Shop, also updated hardcoded users list

--- RoadRangerMobileApp/RoadRangerMobileApp/Models/ShopModel.cs
+++ RoadRangerMobileApp/RoadRangerMobileApp/Models/ShopModel.cs
@@ -8,57 +8,6 @@ namespace RoadRangerMobileApp.Models
 {
     public class ShopModel
     {
-        private readonly AuthorizationService authorization = AuthorizationService.GetInstance();
-
-        public void BuyBoost30p()
-        {
-            int operationStatus = BoostShop.BuyBoost30p(authorization.GetCurrentUser());
-            if(operationStatus == 1)
-            {
-                //Boostas jau galioja
-            }
-            if(operationStatus == 2)
-            {
-                //Nepakanka scoro nusipirkti boostui
-            }
-            else
-            {
-                //Operacija sekminga
-            }
-        }
-
-        public void BuyBoost50p()
-        {
-            int operationStatus = BoostShop.BuyBoost50p(authorization.GetCurrentUser());
-            if (operationStatus == 1)
-            {
-                //Boostas jau galioja
-            }
-            if (operationStatus == 2)
-            {
-                //Nepakanka scoro nusipirkti boostui
-            }
-            else
-            {
-                //Operacija sekminga
-            }
-        }
-
-        public void BuyBoostDouble()
-        {
-            int operationStatus = BoostShop.BuyBoostDouble(authorization.GetCurrentUser());
-            if (operationStatus == 1)
-            {
-                //Boostas jau galioja
-            }
-            if (operationStatus == 2)
-            {
-                //Nepakanka scoro nusipirkti boostui
-            }
-            else
-            {
-                //Operacija sekminga
-            }
-        }
+        
     }
 }
