723270b8f084b8c371f96f4ddb76561ad6f2bc42
Modless <ryrius@gmail.com>
2018-11-15 00:07:37 +0200

    Created API project and added UserController with Authorization

---
+++ RoadRangerWebAPI/Interfaces/IAuthorizationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RoadRangerWebAPI.Models;
+
+namespace RoadRangerWebAPI.Interfaces
+{
+	public interface IAuthorizationService
+	{
+		string Authenticate(string username, string password);
+		User Register(string username, string password);
+	}
+}
---
+++ RoadRangerWebAPI/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RoadRangerWebAPI.Models
+{
+	public class User
+	{
+		[Required]
+		public string ID { get; set; }
+
+		[Required]
+		public string Username { get; set; }
+
+		[Required]
+		public string Password { get; set; }
+
+		[Required]
+		public string Salt { get; set; }
+
+		[Required]
+		public int Score { get; set; }
+
+		public User ShallowCopy()
+		{
+			return (User)this.MemberwiseClone();
+		}
+	}
+}
