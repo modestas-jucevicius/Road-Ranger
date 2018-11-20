d5d03635631031c98eefea9b456357452b59de54
PauliusAn <pauliusanuzas@gmail.com>
2018-11-20 10:37:35 +0200

    user file changes

--- RoadRangerMobileApp/RoadRangerMobileApp/Presenters/MapToolPresenter.cs
+++ RoadRangerMobileApp/RoadRangerMobileApp/Presenters/MapToolPresenter.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms.Maps;
+using Android.Locations;
 
 namespace RoadRangerMobileApp.Presenters
 {
